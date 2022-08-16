8e409b8c4aa4fdd56586057856cdedd680ec9e5a
xiaotian <xiaotian@xingshulin.com>
2022-08-16 21:46:57 +0800

    fix: fix nvim-tree warn

--- lua/user/conf/nvim-tree.lua
+++ lua/user/conf/nvim-tree.lua
@@ -1,26 +1,26 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
+--[[ vim.g.nvim_tree_icons = { ]]
+--[[   default = "", ]]
+--[[   symlink = "", ]]
+--[[   git = { ]]
+--[[     unstaged = "", ]]
+--[[     staged = "S", ]]
+--[[     unmerged = "", ]]
+--[[     renamed = "➜", ]]
+--[[     deleted = "", ]]
+--[[     untracked = "U", ]]
+--[[     ignored = "◌", ]]
+--[[   }, ]]
+--[[   folder = { ]]
+--[[     default = "", ]]
+--[[     open = "", ]]
+--[[     empty = "", ]]
+--[[     empty_open = "", ]]
+--[[     symlink = "", ]]
+--[[   }, ]]
+--[[ } ]]
+--[[]]
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -43,14 +43,14 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = true,
+  --[[ auto_close = true, ]]
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
+  --[[ update_to_buf_dir = { ]]
+  --[[   enable = true, ]]
+  --[[   auto_open = true, ]]
+  --[[ }, ]]
   diagnostics = {
     enable = true,
     icons = {
@@ -76,11 +76,11 @@ nvim_tree.setup {
     open_file = {
       resize_window = true,
       quit_on_open = false,
-    },
-    window_picker = {
-      enable = false,
-      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-      exclude = {},
+      window_picker = {
+        enable = false,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {},
+      },
     },
   },
   filters = {
@@ -97,7 +97,7 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
+    --[[ auto_resize = true, ]]
     mappings = {
       custom_only = false,
       list = {
@@ -113,15 +113,15 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  --[[ quit_on_open = 0, ]]
+  --[[ git_hl = 1, ]]
+  --[[ disable_window_picker = 0, ]]
+  --[[ root_folder_modifier = ":t", ]]
+  --[[ show_icons = { ]]
+  --[[   git = 1, ]]
+  --[[   folders = 1, ]]
+  --[[   files = 1, ]]
+  --[[   folder_arrows = 1, ]]
+  --[[   tree_width = 30, ]]
+  --[[ }, ]]
 }
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -113,6 +113,9 @@ return packer.startup(function(use)
 
   -- startuptime
 
+  -- notify
+  use 'rcarriga/nvim-notify'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
